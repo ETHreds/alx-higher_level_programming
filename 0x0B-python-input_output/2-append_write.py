b5d0315d2499022c7db7a6936c4472c66baf6004
ETHreds <jcmaglaw@gmail.com>
2022-09-27 16:30:36 +0300

    Please work without test

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    Append to file 
+"""
+
+
+def append_write(filename="", text=""):
+    """Appends to text file and returns number odf chars added"""
+    with open(filename, mode='a', encoding='UTF8') as file:
+        for char in text:
+            file.write(char)
+    return len(text)
