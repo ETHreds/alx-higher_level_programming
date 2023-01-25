266425f6fdb85d52982b34e504d729099e1fbef9
ETHreds <jcmaglaw@gmail.com>
2023-01-25 17:31:23 +0300

    neandathal way

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
-// console.log('C is fun');
-// console.log('Python is cool');
-// console.log('JavaScript is amazing');
-const statements = ["C is fun", "Python is cool", "JavaScript is amazing"];
-
-for (const statement of statements) {
-  console.log(statement);
-}
+console.log("C is fun");
+console.log("Python is cool");
+console.log("JavaScript is amazing");
