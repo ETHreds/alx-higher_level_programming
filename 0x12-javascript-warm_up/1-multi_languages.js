e7f2f3b8a9bf4b1bb3e99cc75c08b1b4b6819dd7
ETHreds <jcmaglaw@gmail.com>
2023-01-25 17:28:35 +0300

    for loops for the win

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,9 @@
 #!/usr/bin/node
-console.log('C is fun');
-console.log('Python is cool');
-console.log('JavaScript is amazing');
+// console.log('C is fun');
+// console.log('Python is cool');
+// console.log('JavaScript is amazing');
+const statements = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+for (const statement of statements) {
+  console.log(statement);
+}
